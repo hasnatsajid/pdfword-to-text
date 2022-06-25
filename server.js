6fc04419c4beb81936196bb47323c9ceb6a5fb86
hasnatsajid965 <86589885+hasnatsajid@users.noreply.github.com>
2022-06-25 11:28:56 +0500

    pdf to text

--- server.js
+++ server.js
@@ -121,3 +121,7 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Application started and Listening on port 3000');
 });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
